8d8a30a6a8aaa9ec320e6fd42ea1dc8fa6bb3e73
alloeee <artem993.92016@yandex.ru>
2025-09-26 19:57:40 +0300

    task 4

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main() {
 
@@ -8,10 +9,26 @@ int main() {
 
     std::cout << "$ ";
 
+    const char* home=std::getenv("HOME");
+    std::string historyPath=std::string(home)+"/.kubsh_history";
+    
     std::string input;
     while (std::getline(std::cin, input))
     {
         std::cout << "$ ";
+        if(!input.empty()){
+	    std::ofstream history(historyPath,std::ios::app);
+	    history << input<<"\n";
+ 	}
+	if(input=="history")
+	{
+	    std::ifstream historyOutput(historyPath);
+            std::string line;
+            while(std::getline(historyOutput,line))
+            {
+                std::cout<<line<<"\n";
+            }
+	}
         if (input == "\\q"){
             break;}
     }
