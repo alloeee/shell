330e9f996770f10e4a161632806d569c2c732891
alloeee <artem993.92016@yandex.ru>
2025-09-26 19:25:47 +0300

    task 1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    std::cout << "$ ";
+
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout<<input<<"\n";
+    return 0;
+
+
+
+}
