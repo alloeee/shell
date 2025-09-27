fc5d060c3a7a131af1abe869202b371154cd9567
Artyom <artem993.92016@yandex.ru>
2025-09-27 10:02:57 +0300

    task 2

--- main.cpp
+++ main.cpp
@@ -8,34 +8,9 @@ int main() {
     std::cerr << std::unitbuf;
 
     std::cout << "$ ";
-
-    const char* home=std::getenv("HOME");
-    std::string historyPath=std::string(home)+"/.kubsh_history";
     
     std::string input;
-    while (std::getline(std::cin, input))
-    {
-        std::cout << "$ ";
-        if(!input.empty()){
-	    std::ofstream history(historyPath,std::ios::app);
-	    history << input<<"\n";
- 	}
-	if(input=="history")
-	{
-	    std::ifstream historyOutput(historyPath);
-            std::string line;
-            while(std::getline(historyOutput,line))
-            {
-                std::cout<<line<<"\n";
-            }
-	}
-        else if (input == "\\q"){
-            break;}
-        else if (input.substr(0, 5) == "echo ")
-        {
-            std::cout << input.substr(5)<<"\n";
-        }
- 	else std::cout<<input<<": command not found\n";
-	std::cout<<"$ ";
-    }
+    while(std::getline(std::cin, input))
+	std::cout<<input<<"\n$ ";
 }
+        
