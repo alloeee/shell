d3181c2f5e05d00a60decb6ebb53184af3823837
alloeee <artem993.92016@yandex.ru>
2025-10-17 19:58:43 +0300

    task 7

---
+++ src/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+#include <string>
+
+#include <fstream>
+
+
+
+int main() {
+
+
+
+    std::cout << std::unitbuf;
+
+    std::cerr << std::unitbuf;
+
+
+
+    std::cout << "$ ";
+
+
+
+    const char* home=std::getenv("HOME");
+
+    std::string historyPath=std::string(home)+"/.kubsh_history";
+
+
+
+    std::string input;
+
+    while (std::getline(std::cin, input))
+
+    {
+
+        std::cout << "$ ";
+
+        if(!input.empty()){
+
+	    std::ofstream history(historyPath,std::ios::app);
+
+	    history << input<<"\n";
+
+ 	}
+
+	if(input=="history")
+
+	{
+
+	    std::ifstream historyOutput(historyPath);
+
+            std::string line;
+
+            while(std::getline(historyOutput,line))
+
+            {
+
+                std::cout<<line<<"\n";
+
+            }
+
+	}
+
+        else if (input == "\\q"){
+
+            break;}
+
+        else if (input.substr(0, 5) == "echo ")
+
+        {
+
+
+            std::cout << input.substr(5)<<"\n";
+
+
+        }
+
+else if (input.substr(0,4) == "\\e $")
+{
+    std::string varName = input.substr(4);
+    const char* value = std::getenv(varName.c_str());
+
+   if(value != nullptr)
+    {
+        std::string valueStr = value;
+        
+        bool has_colon = false;
+        for (char c : valueStr)
+        {
+            if (c == ':') 
+            {
+                has_colon = true;
+                break;
+            }
+        }
+        
+        if (has_colon) 
+        {
+            std::string current_part = "";
+            for (char c : valueStr)
+            {
+                if (c == ':') 
+                {
+                    std::cout << "- " << current_part << "\n";
+                    current_part = "";
+                }
+                else 
+                {
+                    current_part += c;
+                }
+            }
+            std::cout << "- " << current_part << "\n";
+        }
+        else 
+        { 
+            std::cout << valueStr << "\n";
+        }
+    }
+    else
+    {
+        std::cout << varName << ": не найдено\n";
+    }
+}
+
+
+
+	else std::cout<<input<<": command not found\n";
+
+
+	std::cout<<"$ ";
+
+    }
+
+}
